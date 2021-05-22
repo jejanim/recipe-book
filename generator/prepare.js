37560362f0306329415f49252a2cef894c9ff458
Lukas Fritsch <lukas.fritsch@outlook.com>
2021-05-22 16:06:36 +0200

    implement story generation

--- generator/prepare.js
+++ generator/prepare.js
@@ -1,13 +1,11 @@
 const fs = require("fs");
-
-const applyFileModifications = () => {
-
-}
+const sourcePath = 'downloaded/recipes'
+const destPath = 'src/stories'
 
 /**
  * Aasdfd
  * @param {string} path 
- * @return {Promise<string[]>}
+ * @return {Promise<{path: string, name: string}>}
  */
 const getFilesInDirectoryRecursive = async (path) => {
   const allFiles = []
@@ -17,7 +15,7 @@ const getFilesInDirectoryRecursive = async (path) => {
   const files = await fs.promises.readdir(path, {withFileTypes: true})
   files.forEach(async (f) => {
     if(f.isFile()){
-      allFiles.push(`${path}/${f.name}`)
+      allFiles.push({path, name: f.name})
     }else if(f.isDirectory()){
       const r = getFilesInDirectoryRecursive(`${path}/${f.name}`)
       .then(entries => allFiles.push(...entries))
@@ -29,8 +27,36 @@ const getFilesInDirectoryRecursive = async (path) => {
   return Promise.all(runners).then(() => allFiles)
 }
 
-const files = getFilesInDirectoryRecursive('downloaded/recipes')
+/**
+ * 
+ * @param {string} path 
+ */
+const getCategories = (path) => {
+  const blacklist = sourcePath.split('/')
+  return path.split('/').filter(c => !blacklist.includes(c) && !c.endsWith('.md'))
+}
+
+/**
+ * Creates a story using the provided file path
+ * @param {{path: string, name: string}} file
+ */
+const createStory = (file) => {
+  const fileName = file.name
+  const categories = getCategories(file.path)
+  const header = 
+  `import { Meta } from '@storybook/addon-docs/blocks';
+  <Meta title="Rezepte/${categories.join('/')}/${fileName}" />\n\n`
+  const content = fs.readFileSync(`${file.path}/${file.name}`)
+
+  fs.writeFileSync(`${destPath}/${fileName}x`, header + content)
+}
+
+getFilesInDirectoryRecursive(sourcePath)
   .then(files => {
     console.log(`RESULTS:`)
-    files.forEach(f => console.log(f))
+    files.forEach(f => {
+      console.log(`${f.path}/${f.name}`)
+      createStory(f)
+      process.exit(0)
+    })
   })
